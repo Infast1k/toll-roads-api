294175c936304ced465c278665b9ab7eaedae3aa
Mihail <infast1k@mail.ru>
2024-09-09 12:31:25 +0300

    init: setup company application

---
+++ company/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# Write your models here
--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'company',
 ]
 
 MIDDLEWARE = [
