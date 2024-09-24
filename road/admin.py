88385c99a43b34bc7cd93f55329f6f47c8d73c26
Mihail <infast1k@mail.ru>
2024-09-24 10:23:06 +0300

    feat(models): create and register Road model

--- road/admin.py
+++ road/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from road.models import Road
+
+
+@admin.register(Road)
+class RoadAdmin(admin.ModelAdmin):
+    list_display = (
+        "oid",
+        "name",
+        "locations",
+        "company__name",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "locations",
+        "company__name",
+        "company__account__email",
+    )
--- road/models.py
+++ road/models.py
@@ -1,3 +1,39 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Road(models.Model):
+    oid = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        db_index=True,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    locations = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    company = models.ForeignKey(
+        to="company.Company",
+        on_delete=models.CASCADE,
+        related_name="roads",
+        db_index=True,
+    )
+    # TODO: think about metadata like distance, duration, max speed and etc.
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.company.name}"
+
+    class Meta:
+        verbose_name = "Road"
+        verbose_name_plural = "Roads"
+        db_table = "roads"
+        ordering = ["-created_at"]
